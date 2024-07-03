5c7675b29c51cde85a7216c569b85fc58876b988
Elliot <ellthrowaway2@icloud.com>
2024-07-03 18:32:16 -0400

    added slice to sorted array to prevent OG array mutation

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const getMean = (array) =>
     array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-    const sorted = array.sort((a, b) => a - b);
+    const sorted = array.slice().sort((a, b) => a - b);
     //
     // first pass using if statement:
     // if (sorted.length % 2 === 0) {
