716daf5e95816a810c9b5e516bc1a29c08db98bd
Elliot <ellthrowaway2@icloud.com>
2024-07-03 17:05:56 -0400

    completed getMean function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const calculate = () => {
-    const value = document.querySelector("#numbers").value
-    const array = value.split(/,\s*/g)
-    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el))
-}
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+    const mean = getMean(numbers);
+    document.querySelector("#mean").textContent = mean;
+};
+
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
