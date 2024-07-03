1f999a79382798e4b94db549c2144314a1a7b946
Elliot <ellthrowaway2@icloud.com>
2024-07-03 18:19:58 -0400

    first pass working through variance logic

--- script.js
+++ script.js
@@ -58,4 +58,7 @@ const getRange = (array) => Math.max(...array) - Math.min(...array);
 
 const getVariance = (array) => {
     const mean = getMean(array);
+    const differences = array.map((el) => el - mean);
+    const squaredDifferences = differences.map((el) => el ** 2);
+    const sumSquaredDifferences = squaredDifferences.reduce((acc, el) => acc + el, 0);
 };
