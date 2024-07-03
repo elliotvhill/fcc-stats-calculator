199a603e1937ee46d67cb8916e1318961b8db1f3
Elliot <ellthrowaway2@icloud.com>
2024-07-03 17:36:04 -0400

    removed example code; completed getMedian function

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const calculate = () => {
     const array = value.split(/,\s*/g);
     const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
     const mean = getMean(numbers);
+    const median = getMedian(numbers);
     document.querySelector("#mean").textContent = mean;
+    document.querySelector("#median").textContent = median;
 };
 
 const getMean = (array) =>
@@ -11,13 +13,21 @@ const getMean = (array) =>
 
 const getMedian = (array) => {
     const sorted = array.sort((a, b) => a - b);
+    //
+    // first pass using if statement:
+    // if (sorted.length % 2 === 0) {
+    //     return getMean([
+    //         sorted[sorted.length / 2] - 1,
+    //         sorted[sorted.length / 2],
+    //     ]);
+    // } else {
+    //     return sorted[Math.floor(sorted.length / 2)];
+    // }
+    //
+    const median =
+        array.length % 2 === 0
+            ? getMean([sorted[sorted.length / 2] - 1, sorted[sorted.length / 2]])
+            : sorted[Math.floor(sorted.length / 2)];
+    return median;
 };
 
-const testArr1 = [1, 2, 3, 4, 5];
-const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length % 2 === 0;
-console.log(isEven);
-const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
-console.log(oddListMedian);
-const evenListMedian = getMean([testArr2[testArr2.length / 2 - 1], testArr2[testArr2.length / 2]]);
-console.log(evenListMedian);
